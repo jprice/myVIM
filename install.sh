b106928cae93e32e9119e07df29ee8fa91a96e4d
alexbel <alexbel@lavabit.com>
2012-04-08 20:26:36 -0400

    add gitkeep files to store swp and backups folders

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@
 echo 'export NODE_PATH=$HOME/.vim/bundle/js-beautify:$NODE_PATH' >> ~/.profile
 export NODE_PATH=$HOME/.vim/bundle/js-beautify:$NODE_PATH
 
-# create environment directories
-mkdir -pv ~/.vim/swp
-mkdir -pv ~/.vim/backups
-
 #symlinks
 ln -sfv ~/.vim/.vimrc ~/.vimrc
 ln -sfv ~/.vim/.jslintrc ~/.jslintrc
